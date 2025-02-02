26f59c4522215beca79257aeb5ab619826b0b01e
MayurNol1 <mayurnol111@gmail.com>
2025-02-03 01:26:36 +0530

    Add cat And Dog api simulatiloly

--- API/Dog and Cat random picture/app.js
+++ API/Dog and Cat random picture/app.js
@@ -1,13 +1,20 @@
 let url = "https://dog.ceo/api/breeds/image/random"
 let url2="https://cataas.com/cat";
 
-let image=document.querySelector('img');
-let btn=document.querySelector('button');
+let imageD=document.querySelector('.Dogimg');
+let imageC=document.querySelector('.Catimg');
+let btns=document.querySelectorAll('button');
 
-btn.addEventListener("click",async ()=>{
+btns[0].addEventListener('click',async function(){
+    let link= await dogPic();
+    console.log(link)
+    imageD.setAttribute("src",link);
+})
+
+btns[1].addEventListener('click',async function(){
     let link= await catPic();
     console.log(link)
-    image.setAttribute("src",url2+"/"+link);
+    imageC.setAttribute("src",`https://cataas.com/cat/${link}`);
 })
 
 async function catPic(){
@@ -22,16 +29,16 @@ async function catPic(){
 }
 
 
-// async function dogPic(){
-//     try{
-//     let img= await axios.get(url);
-//     console.log(img)
-//     return img.data.message;
-//     }
-//     catch(e){
-//         console.log(e);;
-//     }
-// }
+async function dogPic(){
+    try{
+    let img= await axios.get(url);
+    console.log(img)
+    return img.data.message;
+    }
+    catch(e){
+        console.log(e);;
+    }
+}
 
 
 
