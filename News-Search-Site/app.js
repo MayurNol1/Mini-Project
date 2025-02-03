181006c7cce7aed89adca78889292b3a13241536
MayurNol1 <mayurnol111@gmail.com>
2025-02-04 02:00:04 +0530

    All step are done and project is Complete

--- News-Search-Site/app.js
+++ News-Search-Site/app.js
@@ -1,6 +1,6 @@
 const API_KEY ="1cd1cdcdb8184aa99bd228f4e98c908a";
 const url="https://newsapi.org/v2/everything?q=";
-window.addEventListener("load",() => fetchNews("india"));
+window.addEventListener("load",() => fetchNews("India"));
 
 async function fetchNews(query) {
     const res=await fetch(`${url}${query}&apikey=${API_KEY}`);
@@ -37,4 +37,30 @@ function fillDataInCard(clone,article){
     newsTitle.innerHTML=article.title;
     newsDesc.innerHTML=article.description;
     newsSource.innerHTML=`${article.source.name} • ${date}`;
+
+    clone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"_blank");
+    })
 }
+
+let currSelectedItem=null;
+function onNavItemClick(id){
+    searchText.value=null;
+    fetchNews(id);
+    const navItem=document.getElementById(id);
+    currSelectedItem?.classList.remove('active');
+    currSelectedItem= navItem;
+    currSelectedItem.classList.add('active');
+   
+}
+
+const searchText =document.getElementById('news-input');
+const searchBtn=document.getElementById('search-button');
+
+searchBtn.addEventListener('click',()=>{
+    const query=searchText.value;
+    // console.log(query);
+    if(!query) return;
+    fetchNews(query);
+    currSelectedItem?.classList.remove('active');
+})
